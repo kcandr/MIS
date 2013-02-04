42e3091dcb974a54d67d31d638427022b8021284
kcandr <romanchev.aleksandr@gmail.com>
2013-02-05 00:13:37 +0300

    decode algorithm complete

--- 4/main.cpp
+++ 4/main.cpp
@@ -2,28 +2,62 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    string plainText;
-    string codeText;
-    int key[] = {6, 3, 2, 5, 1, 4};
-    cout << "Enter a text: ";
-    getline(cin, plainText);
-    int pos = 0;
-    int size = plainText.size();
-    string subCode = "      ";
+int key[] = {6, 3, 2, 5, 1, 4};
 
-    codeText.reserve(plainText.size());
+string encode(const string &text)
+{
+    int pos = 0;
+    int size = text.size();
+    string subCode = "      ";
+    string encodeText;
+    encodeText.reserve(text.size());
     while (pos < size) {
-        string subPlain = plainText.substr(pos, pos + 6);
+        string subPlain = text.substr(pos, pos + 6);
         for (int i = 0; i < 6; ++i) {
             if (key[i]-1 < subPlain.size())
                 subCode[i] = subPlain[key[i]-1];;
         }
-        codeText += subCode;
+        encodeText += subCode;
         subCode = "      ";
         pos += 6;
     }
-    cout << "Encode text:" << codeText << endl;
+
+    return encodeText;
+}
+
+string decode(const string &encodeText)
+{
+    int pos = 0;
+    int size = encodeText.size();
+    string subCode = "      ";
+    string decodeText;
+    decodeText.reserve(encodeText.size());
+    while (pos < size) {
+        string subPlain = encodeText.substr(pos, pos + 6);
+        for (int i = 0; i < 6; ++i) {
+            if (key[i]-1 < subPlain.size())
+                subCode[key[i]-1] = subPlain[i];;
+        }
+        decodeText += subCode;
+        subCode = "      ";
+        pos += 6;
+    }
+
+    return decodeText;
+}
+
+int main()
+{
+    string plainText;
+    
+    cout << "Enter a text: ";
+    getline(cin, plainText);
+    
+    string encodeText = encode(plainText);
+    cout << "Encode text: " << encodeText << endl;
+
+    string decodeText = decode(encodeText);
+    cout << "Decode text: " << decodeText << endl;
+
     return 1;
 }
